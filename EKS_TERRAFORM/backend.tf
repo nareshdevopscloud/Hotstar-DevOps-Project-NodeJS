84dc9d266843b1ec52ebe784d875f14491dd320d
nareshdevopscloud <150413361+nareshdevopscloud@users.noreply.github.com>
2024-02-04 13:53:02 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "naresh-salar" # Replace with your actual S3 bucket name
+    bucket = "mys3bucket-hotstar" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
-    region = "us-east-1"
+    region = "ap-south-1"
   }
 }
